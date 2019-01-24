7bf9e0a34bf1ba288474abfc0ec56dfff76661c2
Fumiya <fumiyan.uec@gmail.com>
2019-01-24 17:18:40 +0900

    a

--- csrc/ccnd/ccnd_main.c
+++ csrc/ccnd/ccnd_main.c
@@ -163,7 +163,6 @@ static int
 stdiologger(void *loggerdata, const char *format, va_list ap)
 {
     FILE *fp = (FILE *)loggerdata;
-    vfprintf(stdout, format, ap);
     return(vfprintf(fp, format, ap));
 }
 
