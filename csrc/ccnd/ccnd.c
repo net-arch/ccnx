509e9cf2259df4d13d3ed1b2f1e1e34b552c2738
Fumiya <fumiyan.uec@gmail.com>
2019-01-30 15:20:43 +0900

    a

--- csrc/ccnd/ccnd.c
+++ csrc/ccnd/ccnd.c
@@ -2305,6 +2305,7 @@ face_send_queue_insert_qos(struct ccnd_handle *h,struct face *face, struct conte
         for(i = 0;i<100;i++){
             if(face->content_names[i] == NULL){
                 face->content_names[i] = s;
+                ccnd_msg(h,"other content!");
                 break;
             }
             if(strcmp(face->content_names[i],s)==0){
