bc5ba9377aaf701d9788a93af560938139081ce3
Fumiya <fumiyan.uec@gmail.com>
2019-01-30 11:20:35 +0900

    fix

--- csrc/ccnd/ccnd.c
+++ csrc/ccnd/ccnd.c
@@ -755,7 +755,7 @@ finalize_face(struct hashtb_enumerator *e)
         ccn_charbuf_destroy(&face->inbuf);
         ccn_charbuf_destroy(&face->outbuf);
         for (c = 0; c< 100 ;c++)
-            ccn_charbuf_destroy(h,&(face->gList[c]->content_name));
+            ccn_charbuf_destroy(&face->gList[c]->content_name);
         ccnd_msg(h, "%s face id %u (slot %u)",
             recycle ? "recycling" : "releasing",
             face->faceid, face->faceid & MAXFACES);
@@ -7000,6 +7000,8 @@ ccnd_destroy(struct ccnd_handle **pccnd)
             content_queue_destroy(h, &(h->face0->q[i]));
         for (i = 0; i < CCND_FACE_METER_N; i++)
             ccnd_meter_destroy(&h->face0->meter[i]);
+        for (i = 0; i< 100 ;i++)
+            ccn_charbuf_destroy(&h->face0->gList[i]->content_name)
         content_queue_destroy(h,&(h->face0->g_queue));
         content_queue_destroy(h,&(h->face0->d_queue));
         free(h->face0);
